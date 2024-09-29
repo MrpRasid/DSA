e317f52ecbfb0b7fce7164962dd9ad95353860d3
Mamun Rasid Prodhan <mrprasid47@gmail.com>
2024-09-29 11:20:13 +0530

    first commit

---
+++ areaOfcircle.cpp
@@ -0,0 +1,12 @@
+#include<iostream> 
+using namespace std;
+
+int main(){
+    int radius;
+
+    cin>>radius;
+
+    float areaOfcircle = 3.14*radius*radius;
+
+    cout<<"Area of Circle is: "<<areaOfcircle;
+}
